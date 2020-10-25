ca3dc61d5a96fdb23fad026022a55486cc18354a
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-10-25 16:09:18 +0100

    test: test method

---
+++ utils/getS3Data.js
@@ -0,0 +1,16 @@
+import AWS from 'aws-sdk';
+export const getS3Data = async (configParam) => {
+  AWS.config.update({
+    accessKeyId: `${process.env.AWS_ACCESS_KEY_ID1}`,
+    secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY1}`,
+  });
+  const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {
+      Bucket: 'dev-nextenhanced-config',
+    },
+  });
+  const data = await s3.getObject({ Bucket: 'dev-nextenhanced-config', Key: 'next-s3-config.json' }).promise();
+
+  return JSON.parse(data.Body.toString('utf8'))[configParam];
+};
