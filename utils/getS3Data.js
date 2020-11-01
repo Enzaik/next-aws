370fad96034c3cfe40b5ce81e51d8bf2e9fac976
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-11-01 17:37:50 +0100

    feat: rollback to s3 config

--- utils/getS3Data.js
+++ utils/getS3Data.js
@@ -1,21 +1,21 @@
-// import AWS from 'aws-sdk';
-import dataJson from '../next-s3-config.json';
+import AWS from 'aws-sdk';
+// import dataJson from '../next-s3-config.json';
 
 export const getS3Data = async (configParam) => {
-  // AWS.config.update({
-  //   accessKeyId: `${process.env.AWS_ACCESS_KEY_ID1}`,
-  //   secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY1}`,
-  // });
+  AWS.config.update({
+    accessKeyId: `${process.env.AWS_ACCESS_KEY_ID1}`,
+    secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY1}`,
+  });
 
-  // const s3 = new AWS.S3({
-  //   apiVersion: '2012-10-17',
-  //   params: {
-  //     Bucket: 'dev-nextenzaik-config',
-  //     Region: 'eu-west-1',
-  //   },
-  // });
-  // const data = await s3.getObject({ Bucket: 'dev-nextenzaik-config', Key: 'next-s3-config.json' }).promise();
+  const s3 = new AWS.S3({
+    apiVersion: '2012-10-17',
+    params: {
+      Bucket: 'dev-nextenhanced-config',
+      Region: 'eu-west-1',
+    },
+  });
+  const data = await s3.getObject({ Bucket: 'dev-nextenhanced-config', Key: 'next-s3-config.json' }).promise();
 
-  // return JSON.parse(data.Body.toString('utf8'))[configParam];
-  return dataJson[configParam];
+  return JSON.parse(data.Body.toString('utf8'))[configParam];
+  // return dataJson[configParam];
 };
