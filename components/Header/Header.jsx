84b97f5c2319a35565ebdf0457bd38f572f125ea
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-11-12 21:41:30 +0100

    feat: implement search

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ content }) {
   return (
     <header className="bg-white shadow pt-15">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
         <h1 className="text-3xl font-bold leading-tight text-gray-900">{content}</h1>
       </div>
     </header>
