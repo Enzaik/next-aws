7de895ba1d71768612def3455a2fec705c2026c3
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-11-14 10:59:32 +0100

    feat: change some css

--- components/Content/Content.jsx
+++ components/Content/Content.jsx
@@ -19,7 +19,7 @@ function Content({ currentPageId, pageData }) {
 
   return (
     <main className="h-screen bg-gray-100">
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-6 pt-0 sm:px-6 lg:px-8">
         {/* <!-- Replace with your content --> */}
         {renderContent(currentPageId)}
         {/* <!-- /End replace --> */}
